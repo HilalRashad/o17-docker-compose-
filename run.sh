d04b564a9c0a23d05549c385e4b734c5e7dabefd
HilalRashad <35407338+HilalRashad@users.noreply.github.com>
2024-12-18 20:53:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10017/'$PORT'/g' $DESTINATION/Docker-Compose-Enhanced.yaml
-sed -i 's/20017/'$CHAT'/g' $DESTINATION/Docker-Compose-Enhanced.yaml
+sed -i 's/10017/'$PORT'/g' $DESTINATION/docker-compose.yaml
+sed -i 's/20017/'$CHAT'/g' $DESTINATION/docker-compose.yaml
 # run Odoo
-docker-compose -f $DESTINATION/Docker-Compose-Enhanced.yaml up -d
+docker-compose -f $DESTINATION/docker-compose.yaml up -d
 
 echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: odoo17@2023 | Live chat port: '$CHAT
